8dcfc90f4b926f9709ddabade484690d232a71c4
L-Kasu <lars@kasueschke.de>
2025-01-05 17:21:02 +0100

    before rebase

--- tests/test_util/cases/generate_cases.py
+++ tests/test_util/cases/generate_cases.py
@@ -1,12 +1,12 @@
-from jax import random
+import jax
+import random
 from jax import numpy as jnp
 
 
 # Helper-Funktion zur dynamischen Generierung von Pytrees
-def generate_random_pytree(depth=2, max_branches=3):
+def generate_random_pytree(depth=2, max_branches=3, seed=1):
     if depth == 0:
         return jnp.array(random.randint(0, 10))
-
     pytree_type = random.choice(["dict", "list", "tuple"])
     if pytree_type == "dict":
         return {f"key_{i}": generate_random_pytree(depth - 1) for i in range(random.randint(1, max_branches))}
@@ -16,3 +16,4 @@ def generate_random_pytree(depth=2, max_branches=3):
         return tuple(generate_random_pytree(depth - 1) for _ in range(random.randint(1, max_branches)))
 
 
+a = generate_random_pytree(3, 8)
